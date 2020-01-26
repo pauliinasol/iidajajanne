6f2114cb3bae182733e582fa2f29df867714b8ca
Pauliina Solanne <pauliina.solanne@hoxhunt.com>
2020-01-26 18:00:08 +0200

    map

--- src/Components/Map.js
+++ src/Components/Map.js
@@ -9,7 +9,7 @@ export const Map = () => {
   return (
     <MapContainer>
       <iframe
-        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d15301.12361199119!2d22.900630657375952!3d61.355061324417356!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x46893475b9e1d64b%3A0xa00b553b98f0fb0!2sKalliala%2C%2038210%20Sastamala!5e0!3m2!1sfi!2sfi!4v1580021767888!5m2!1sfi!2sfi"
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1996.8659966580353!2d23.762818916098137!3d59.96754628188667!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x468da0df6cbe5e67%3A0x334f8183406a785!2sCalliola%20Conference%20%26%20Event%20Center!5e0!3m2!1sfi!2sfi!4v1580054360088!5m2!1sfi!2sfi"
         width="100%"
         height="450"
         frameborder="0"
