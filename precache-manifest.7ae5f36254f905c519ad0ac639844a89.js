16b9610001d64f607a576b649a9736964275a757
Pauliina Solanne <pauliina.solanne@hoxhunt.com>
2020-02-04 06:47:21 +0200

    Updates

---
+++ precache-manifest.7ae5f36254f905c519ad0ac639844a89.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2540c11e9207c7794d00960c41d69b2",
+    "url": "/iidajajanne/index.html"
+  },
+  {
+    "revision": "0c17a2ba05f119137818",
+    "url": "/iidajajanne/static/css/2.7abef457.chunk.css"
+  },
+  {
+    "revision": "4557621094cae5dac35f",
+    "url": "/iidajajanne/static/css/main.f662d330.chunk.css"
+  },
+  {
+    "revision": "0c17a2ba05f119137818",
+    "url": "/iidajajanne/static/js/2.57625e1c.chunk.js"
+  },
+  {
+    "revision": "4557621094cae5dac35f",
+    "url": "/iidajajanne/static/js/main.3fbafd2a.chunk.js"
+  },
+  {
+    "revision": "8eb4118046e9b8257c9f",
+    "url": "/iidajajanne/static/js/runtime-main.9a801940.js"
+  },
+  {
+    "revision": "c21282da55a646a1bff951374f48bcf0",
+    "url": "/iidajajanne/static/media/iidajajannepicedited.c21282da.png"
+  },
+  {
+    "revision": "c12dae98b0f18d2a724d41779ca93cb9",
+    "url": "/iidajajanne/static/media/kihlauskuva.c12dae98.png"
+  }
+]);
